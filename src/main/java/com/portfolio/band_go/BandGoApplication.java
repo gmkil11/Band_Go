e461da7b2064bffaad1d677ca9a0e9023a4bc6f9
gmkil11 <mkkil4451@gmail.com>
2024-03-11 17:11:00 +0900

    Feat : 기본 설정

---
+++ src/main/java/com/portfolio/band_go/BandGoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.band_go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/band_go/BandGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.band_go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
