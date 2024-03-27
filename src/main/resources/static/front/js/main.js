b31050eec09e3c7e617062eb617aa078e8b1f5ca
gmkil11 <mkkil4451@gmail.com>
2024-03-28 00:33:08 +0900

    Feat: 로그인 정보 조회 기능 추가 Feat: main.js를 통해 헤더 영역에서 session 로그인 정보 조회 가능 기능 추가

---
+++ src/main/resources/static/front/js/main.js
@@ -0,0 +1,16 @@
+window.onload = async function() {
+
+
+    const { data: { user } } = await client.auth.getUser()
+
+    if (user) {
+        const uid = user.user_metadata.user_name;
+        console.log(user);
+        const userInfoDiv = document.getElementById('user-info');
+        userInfoDiv.textContent = uid + "님 안녕하세요";
+    } else {
+        console.log('session is none');
+    }
+
+
+}
