eb9a77b3385c7429c26001e2fe05524934c9848d
gmkil11 <mkkil4451@gmail.com>
2024-03-29 23:46:20 +0900

    Feat: 로그인 기능 추가중

---
+++ src/main/resources/static/front/js/login.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+
+    console.log("로그인 페이지 진입.")
+
+    loginForm.addEventListener('submit', async function(event ) {
+        event.preventDefault();
+        console.log(email.value, password.value);
+        const {data, error} = await client.auth.signInWithPassword({
+            email: email.value,
+            password: password.value,
+        })
+
+        if (error) {
+            console.error('로그인에 실패했습니다:', error.message);
+        } else {
+            console.log('로그인이 완료되었습니다:', data);
+            // 회원가입 성공 시 다음 페이지로 이동하거나 다른 동작을 수행할 수 있습니다.
+            signupButton.setAttribute('type', 'submit'); // 버튼 타입을 submit으로 변경
+        }
+    });
+});
--- src/main/resources/static/front/js/signup.js
+++ src/main/resources/static/front/js/signup.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const confirmPasswordInput = document.getElementById('confirmPassword');
     const passwordMatchMessage = document.getElementById('passwordMatchMsg');
     const signupForm = document.getElementById('signupForm');
+    const signupButton = document.getElementById('submitButton');
 
     function validatePassword() {
         if (passwordInput.value !== confirmPasswordInput.value) {
@@ -19,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
     confirmPasswordInput.addEventListener('input', validatePassword);
 
     signupForm.addEventListener('submit', async function(event) {
+        signupButton.setAttribute('type', ''); // 버튼 타입을 null로 변경
         event.preventDefault();
         validatePassword(); // 비밀번호 일치 여부 검증
 
@@ -40,6 +42,7 @@ document.addEventListener('DOMContentLoaded', function() {
             } else {
                 console.log('회원가입이 완료되었습니다:', user);
                 // 회원가입 성공 시 다음 페이지로 이동하거나 다른 동작을 수행할 수 있습니다.
+                signupButton.setAttribute('type', 'submit'); // 버튼 타입을 submit으로 변경
             }
         }
     });
