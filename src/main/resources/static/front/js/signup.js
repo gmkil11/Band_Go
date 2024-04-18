97ac3615af51c2374967c461db3896b532d74a99
gmkil11 <mkkil4451@gmail.com>
2024-04-18 21:50:23 +0900

    Fix: 로그인 페이지 validation 처리 작업 완료

--- src/main/resources/static/front/js/signup.js
+++ src/main/resources/static/front/js/signup.js
@@ -1,13 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     const nameInput = document.getElementById('name')
-    const phoneInput = document.getElementById('phone')
+    const nameInputBox = document.querySelector(".name_input_box");
     const emailInput = document.getElementById('email');
+    const emailInputBox = document.querySelector(".email_input_box");
+    const phoneInput = document.getElementById('phone')
+    const phoneInputBox = document.querySelector(".phone_input_box");
     const passwordInput = document.getElementById('password');
+    const passwordInputBox = document.querySelector(".password_input_box");
     const confirmPasswordInput = document.getElementById('confirmPassword');
+    const pwCheckInputBox = document.querySelector(".pwCheck_input_box");
     const passwordMatchMessage = document.getElementById('passwordMatchMsg');
     const signupForm = document.getElementById('signupForm');
     const signupButton = document.getElementById('submitButton');
+
+
 
     function validatePassword() {
         if (passwordInput.value !== confirmPasswordInput.value) {
@@ -18,12 +25,57 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
+
+
+
+
     confirmPasswordInput.addEventListener('input', validatePassword);
 
     signupForm.addEventListener('submit', async function(event) {
         signupButton.setAttribute('type', ''); // 버튼 타입을 null로 변경
         event.preventDefault();
         validatePassword(); // 비밀번호 일치 여부 검증
+
+
+        if (nameInput.value === "" || emailInput.value === "" || passwordInput.value === "" || confirmPasswordInput.value === "" || phoneInput.value === "" ){
+            if(nameInput.value === "") {
+                if (emailInputBox) {
+                    nameInputBox.style.border = 'darkred solid 1px';
+                    console.log("사용자명 입력")
+                } else {
+                    nameInputBox.style.border = 'black solid 1px'
+                }
+            } if (emailInput.value === "") {
+                if(emailInput) {
+                    emailInputBox.style.border = 'darkred solid 1px';
+                    console.log("이메일 입력")
+                } else {
+                    emailInputBox.style.border = 'black solid 1px';
+                }
+            } if (passwordInput.value === "") {
+                if (passwordInputBox) {
+                    passwordInputBox.style.border = 'darkred solid 1px';
+                    console.log("비밀번호 입력")
+                } else {
+                    passwordInputBox.style.border = 'black solid 1px';
+                }
+            } if (confirmPasswordInput.value === "") {
+                if (pwCheckInputBox) {
+                    pwCheckInputBox.style.border = 'darkred solid 1px';
+                    console.log("비밀번호 확인 입력")
+                } else {
+                    pwCheckInputBox.style.border = 'black solid 1px';
+                }
+            } if (phoneInput.value === "") {
+                if (phoneInputBox) {
+                    console.log("휴대전화 입력")
+                    phoneInputBox.style.border = 'darkred solid 1px';
+                } else {
+                    phoneInputBox.style.border = 'black solid 1px'
+                }
+            }
+
+        }
 
         if (passwordInput.value === confirmPasswordInput.value) {
             const { user, error } = await client.auth.signUp({
