8c580d3e5d45dc32c615b20a7f5c24144fede759
gmkil11 <mkkil4451@gmail.com>
2024-07-14 00:06:57 +0900

    Fix: datetimepicker 사용자가 직접적으로 input에 접근하지 못 하도록 막음

--- src/main/resources/static/front/js/calender.js
+++ src/main/resources/static/front/js/calender.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   $(document).ready(function () {
     const startTime = document.getElementById("startTime");
     const endTime = document.getElementById("endTime");
-    $.datetimepicker.setLocale("kr");
+    $.datetimepicker.setLocale("en");
     $("#startTime").datetimepicker({
       step: 30,
       minDate: 0,
@@ -11,5 +11,29 @@ document.addEventListener("DOMContentLoaded", function () {
       step: 30,
       minDate: startTime.value,
     });
+  });
+
+  // 초기화할 날짜 및 시간 형식
+  const initialDate = new Date();
+  const formattedDate = initialDate
+    .toISOString()
+    .slice(0, 16)
+    .replace("T", " ");
+
+  // DateTimePicker 초기화
+  $("#datetimepicker").datetimepicker({
+    format: "Y-m-d H:i", // 원하는 날짜 및 시간 형식 설정
+    step: 30, // 시간 간격 설정 (예: 30분 간격)
+    value: formattedDate, // 초기값 설정
+    onChangeDateTime: function (dp, $input) {
+      // input 값이 변경될 때 span의 innerHTML 업데이트
+      const selectedDate = $input.val();
+      $("#datetime-display").text(selectedDate);
+    },
+  });
+
+  // 아이콘 클릭 시 DateTimePicker 표시
+  $("#datetimepicker-icon").on("click", function () {
+    $("#datetimepicker").datetimepicker("show"); // DateTimePicker 표시
   });
 });
